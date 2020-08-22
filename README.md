cb015b9986e3c9ea6d617bca7871399bf9770dee
Anton Istomin <Anton.Istomin@ibm.com>
2020-08-22 17:13:16 +0300

    Include fuego libs

---
+++ README.md
@@ -0,0 +1,5 @@
+# Installation
+
+1. Install fuego http://fuego.sourceforge.net/
+1. Set environment variable `FUEGO_DIR` to `/path/to/fuego`
+1. Build and run
--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <string>
 #include "test.cpp"
+#include <GoUtil.h>
 
 int main(int argc, char *argv[]) {
     test();
+
+    std::string qwe = GoUtil::ScoreToString(1.2);
+
+    std::cout << qwe << std::endl;
 
     std::cout << "Hello world of Go!" << std::endl;
 
