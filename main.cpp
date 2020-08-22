6fdd5032f8534823cbb178da655661fe6886c8a8
Anton Istomin <Anton.Istomin@ibm.com>
2020-08-22 19:01:46 +0300

    Add libfuego, instantiate GoBoard

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <string>
 #include "test.cpp"
-#include <GoUtil.h>
+#include <GoBoard.h>
+#include <SgInit.h>
+#include <GoInit.h>
 
 int main(int argc, char *argv[]) {
     test();
 
-    std::string qwe = GoUtil::ScoreToString(1.2);
+    SgInit();
+    GoInit();
 
-    std::cout << qwe << std::endl;
+    auto board = new GoBoard(9);
+
+    board->Play({1, 1});
 
     std::cout << "Hello world of Go!" << std::endl;
 
