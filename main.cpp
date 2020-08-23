58557ed2553800af7fa30f9ff0ebbf335bd269e4
Anton Istomin <Anton.Istomin@ibm.com>
2020-08-23 22:05:46 +0300

    Add GtpEngine for testing

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <SgTimeRecord.h>
 #include <SpRandomPlayer.h>
 #include <GoUctSearch.h>
+#include <GoGtpEngine.h>
 
 class PerfectPlayer : GoUctPlayer<GoUctGlobalSearch<GoUctPlayoutPolicy<GoUctBoard>,
         GoUctPlayoutPolicyFactory<GoUctBoard> >,
@@ -36,21 +37,29 @@ int main(int argc, char *argv[]) {
     SgInit();
     GoInit();
 
+    auto engine = new GoGtpEngine(0, 0, false, false);
+
+    const auto& game = engine->Game();
+
+    std::cout << "Current move: " << game.CurrentMove() << std::endl;
+    std::cout << "Game result: " << game.GetResult() << std::endl;
+
     auto board = new GoBoard(9);
 
     auto perfectPlayer = new PerfectPlayer(*board);
 
-    auto randomPlayer = new SpRandomPlayer(*board);
-
     auto time = SgTimeRecord(true, 1.);
 
-    auto point = randomPlayer->GenMove(time, 1);
+    auto point = perfectPlayer->GenMove(time, 1);
 
     std::cout << "Point: " << point << std::endl;
 
     board->Play(point, SG_BLACK);
 
     std::cout << "Hello world of Go!" << std::endl;
+
+    GoFini();
+    SgFini();
 
     return 0;
 }
